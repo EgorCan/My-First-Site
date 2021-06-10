6295b38db5128b4af9773176abcde0c709be0a8d
EgorCan <klochkov.mavrina@gmail.com>
2021-06-09 22:45:26 -0500

    everything

---
+++ js/index4.js
@@ -0,0 +1,14 @@
+let firstName = "Egor";
+console.log(firstName)
+
+let lastName = "Klochkov";
+console.log(lastName)
+
+let age = 35;
+console.log(age)
+
+let dateOfBirth = "01/10/1985";
+console.log(dateOfBirth)
+
+let address = "Victoria Ave"
+console.log(address)
---
+++ js/index5.js
@@ -0,0 +1,14 @@
+let firstName = "Egor";
+console.log(`firstName: ${firstName}`)
+
+let lastName = "Klochkov";
+console.log(`lastName: ${lastName}`)
+
+let age = 35;
+console.log(`age: ${age}`)
+
+let dateOfBirth = "01/10/1985";
+console.log(`dateOfBirth: ${dateOfBirth}`)
+
+let address = "Victoria Ave"
+console.log(`address: ${address}`)
